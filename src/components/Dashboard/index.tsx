7be505db230002df070aba2a21b7b4f7f9d79fba
RafaelBorges93 <88861319+RafaelBorges93@users.noreply.github.com>
2022-03-14 14:02:27 -0300

    project finished

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Summary } from "../Summary";
+import { Container } from "./styles"; 
+import { TransactionsTable } from '../TransactionsTable'
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Summary/>
+      <TransactionsTable />
+    </Container> 
+  );
+}
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,43 @@
+import { Container } from "./styles";
+import { useTransactions } from "../../hooks/useTransactions";
+
+export function TransactionsTable() {
+  const { transactions } = useTransactions();
+  
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {transactions.map(transactions => {
+            return (
+              <tr key={transactions.id}>
+                <td>{transactions.title}</td>
+                <td className={transactions.type}>
+                  {new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                  }).format(transactions.amount)}
+                  </td>
+                <td>{transactions.category}</td>
+                <td>
+                {new Intl.DateTimeFormat('pt-BR').format(
+                  new Date(transactions.createdAt)
+                  )}                  
+                </td>
+              </tr>
+            );
+          })}                
+        </tbody>
+      </table>
+    </Container>
+  )
+}
